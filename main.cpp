9a15bc7357564db25a6741af66b0e78a7ce0ca8d
Martin Molema <Martin.Molema@nhlstenden.com>
2025-02-03 11:40:20 +0100

    improved output and EOF-detection

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <iomanip>
 
 double calculate(double operand1, char op, double operand2) {
     switch (op) {
@@ -20,28 +21,32 @@ double calculate(double operand1, char op, double operand2) {
 int main() {
     std::string input;
     std::regex expression_regex("\\s*(-?\\d*\\.?\\d+)\\s*([+\\-*/])\\s*(-?\\d*\\.?\\d+)\\s*");
+
     std::smatch match;
 
-    std::cout << "Enter an expression (e.g., 5.5 + 3.2) or press Ctrl+D to exit: " << std::endl;
+    bool eofDetected = false;
 
-    while (std::getline(std::cin, input)) {
-        if (std::regex_match(input, match, expression_regex)) {
-            double operand1 = std::stod(match[1]);
-            char op = match[2].str()[0];
-            double operand2 = std::stod(match[3]);
+    while (! eofDetected) {
+        std::getline(std::cin, input);
+        eofDetected = std::cin.eof();
+        if (!eofDetected) {
+            if (std::regex_match(input, match, expression_regex)) {
+                double operand1 = std::stod(match[1]);
+                char op = match[2].str()[0];
+                double operand2 = std::stod(match[3]);
 
-            try {
-                double result = calculate(operand1, op, operand2);
-                std::cout << operand1 << " " << op << " " << operand2 << " = " << result << std::endl;
-            } catch (const std::runtime_error& e) {
-                std::cerr << e.what() << std::endl;
+                try {
+                    double result = calculate(operand1, op, operand2);
+                    std::cout << std::fixed << std::setprecision(3) << operand1 << " " << op << " " << operand2 << " = " << result << std::endl;
+                } catch (const std::runtime_error &e) {
+                    std::cerr << e.what() << std::endl;
+                }
+            } else {
+                std::cerr << "Invalid input format. Use the format: number operator number (e.g., 5.5 + 3.2)"
+                          << std::endl;
             }
-        } else {
-            std::cerr << "Invalid input format. Use the format: number operator number (e.g., 5.5 + 3.2)" << std::endl;
         }
-        std::cout << "Enter another expression or press Ctrl+D to exit: " << std::endl;
     }
 
-    std::cout << "Goodbye!" << std::endl;
     return 0;
 }
